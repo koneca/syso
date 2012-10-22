7c0434ba71d32c851a0956127426faea29c69d82
Konrad Grzeca <kon-eca@koneca.de>
2012-10-22 08:57:31 +0200

    edited init program

--- initapp.c
+++ initapp.c
@@ -1,21 +1,19 @@
 
 #include <stdio.h>
 #include <sys/sysinfo.h>
+#include <unistd.h>     // sysconf
 
 int main() {
 	struct sysinfo info;
 	sysinfo(&info);
-	
+
 	printf("Hello Kernel World\n");
 	printf("Uptime: %lu\n", info.uptime);
-	printf("Total RAM: %lu\n", info.totalram);
-	printf("Free RAM: %lu\n", info.freeram);
+	printf("Total RAM: %lu MB\n", info.totalram/1024/1024);
+	printf("Free RAM: %lu MB\n", info.freeram/1024/1024);
 	printf("Process count: %d\n", info.procs);
-	printf("Page size: %d\n", info.mem_unit);
-	printf("Going to sleep");
-	while (1) {
-		sleep(1000);
-		printf(".");
-	}
+	printf("Page size: %li\n", sysconf(_SC_PAGESIZE));
+	printf("Going to sleep for 5secs\n");
+		usleep (5000000);
 	return 0;
 }
